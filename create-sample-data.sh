911e5993e01ad094c41174363dc954649da36b0e
Lars Kellogg-Stedman <lars@redhat.com>
2017-03-04 20:14:25 -0500

    more verbose output

--- create-sample-data.sh
+++ create-sample-data.sh
@@ -2,6 +2,8 @@
 
 : ${MEMBER_ROLE:=_member_}
 
+roles='manager'
+
 project1_members='user1 user2 user3'
 project2_members='user3 user4'
 project1_managers='user5'
@@ -9,9 +11,15 @@ project2_managers='user6'
 
 set -e
 
-openstack role create --or-show manager
+echo "creating roles"
+for role in $roles; do
+    echo "creating role $role"
+    openstack role create --or-show $role
+done
 
+echo "creating users"
 for user in {1..6}; do
+    echo "creating user user${user}"
     openstack user create --or-show \
         --password secret${user} \
         --email user${user}@example.com \
@@ -19,22 +27,29 @@ for user in {1..6}; do
         user${user}
 done
 
+echo "creating projects"
 for proj in {1..2}; do
+    echo "creating project project${proj}"
     openstack project create --or-show project${proj}
 
     members="project${proj}_members"
     managers="project${proj}_managers"
+
+    echo "adding members to project project${proj}"
     for user in ${!members}; do
+        echo "adding user $user as member to project project${proj}"
         openstack role add \
             --project project${proj} \
             --user $user \
             $MEMBER_ROLE
     done
+
+    echo "adding manager to project project${proj}"
     for user in ${!managers}; do
+        echo "adding user $user as manager to project project${proj}"
         openstack role add \
             --project project${proj} \
             --user $user \
             manager
     done
 done
-
